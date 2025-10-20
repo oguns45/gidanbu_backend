6fdab6c9c3415f1aa56c3db2149572c3397b6a2e
unknown <oguns45@gmail.com>
2025-10-20 13:45:06 +0100

    new stuff

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { login, logout, register, refreshToken, getProfile, uploadAvatarController, changePassword, getAllUsers, updateOnlineStatus } from "../controllers/auth.controller";
-import { protectRoute } from "../middleware/auth.mw";
+import {adminRoute, protectRoute } from "../middleware/auth.mw";
 // import { updateProfile } from './controllers/user.controller';
 import { uploadAvatar } from "../middleware/upload.mw";
 import multer from "multer";
@@ -11,6 +11,8 @@ const router = express.Router();
 router.post("/auth/register", register);
 router.post("/auth/login", login);
 router.post("/auth/logout", logout);
+router.post("/auth/admin/login", login);
+router.post("/auth/admin/logout", logout);
 router.post("/auth/refresh-token", refreshToken);
 router.get("/auth/profile", protectRoute, getProfile);
 router.post('/auth/change-password' , protectRoute, changePassword);
